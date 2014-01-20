c9d4cdd53ddcba77141dd6b1b78372324e958a71
JP Berti <jpberti@yahoo.com>
2014-01-20 15:06:46 -0800

    Initial Commit

---
+++ IPS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IPS
+//
+//  Created by Jon Paul Berti on 1/20/14.
+//  Copyright (c) 2014 JPBerti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IPSTests/IPSTests.m
@@ -0,0 +1,34 @@
+//
+//  IPSTests.m
+//  IPSTests
+//
+//  Created by Jon Paul Berti on 1/20/14.
+//  Copyright (c) 2014 JPBerti. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IPSTests : XCTestCase
+
+@end
+
+@implementation IPSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
