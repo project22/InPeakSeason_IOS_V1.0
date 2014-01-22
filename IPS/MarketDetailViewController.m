b353dc2a87e5047b6b4220a9a09f62cca8d7593d
JP Berti <jpberti@yahoo.com>
2014-01-21 19:17:22 -0800

    fix map to add marker for market

--- IPS/MarketDetailViewController.m
+++ IPS/MarketDetailViewController.m
@@ -27,21 +27,51 @@
     NSDictionary *marketDetails = [market getMarketDetails:self.marketID];
     NSLog(@"Market details are: %@", marketDetails);
     
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [geocoder geocodeAddressString:[marketDetails  valueForKeyPath:@"marketdetails.Address"] completionHandler:^(NSArray* placemarks, NSError* error){
+        for (CLPlacemark* aPlacemark in placemarks)
+        {
+            // Process the placemark.
+            MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+            point.coordinate = aPlacemark.location.coordinate;
+            point.title = self.marketName;
+//            point.subtitle = @"Cool";
+            
+            [self.mapView addAnnotation:point];
+            
+            MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(aPlacemark.location.coordinate, 1000, 1000);
+            [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+            
+        }
+    }];
+    
+    self.navigationItem.title = self.marketName;
+
     self.marketNameLabel.text = self.marketName;
-    // get the pieces out of the dictionary object.  "marketdetail" is root object.
-//    [result valueForKeyPath:@"location.name"];
     self.outputAddressLabel.text = [marketDetails  valueForKeyPath:@"marketdetails.Address"];
     self.outputProductsLabel.text = [marketDetails  valueForKeyPath:@"marketdetails.Products"];
     self.outputScheduleLabel.text = [marketDetails  valueForKeyPath:@"marketdetails.Schedule"];
     
 }
 
-- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
-{
-    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
-    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
-    NSLog(@"I should show my location now");
-}
+//- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+//{
+////    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+////    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+//    
+//    Market *market = [[Market alloc] init];
+//    NSDictionary *marketDetails = [market getMarketDetails:self.marketID];
+//    
+//    
+//    
+//}
+
+//- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+//{
+//    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+//    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+//    NSLog(@"I should show my location now");
+//}
 
 - (void)didReceiveMemoryWarning
 {
